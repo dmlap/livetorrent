22f8172cff34759896664c8f3be55ff2415d9043
Dave LaPalomento <dlapalomento@gmail.com>
2021-01-19 10:30:08 -0500

    Progress display in the sandbox page Update the progress display so that seconds buffered is displayed (instead of bytes downloaded). Remove portions of the UI that do not seem easily translatable to live streaming.

--- src/demo-player.mjs
+++ src/demo-player.mjs
@@ -1,4 +1,5 @@
-/* global fetch, MediaSource, m3u8Parser */
+/* eslint-env browser */
+/* global m3u8Parser */
 import '../../node_modules/m3u8-parser/dist/m3u8-parser.js'
 import LiveTorrent from './index.js'
 
@@ -26,19 +27,19 @@ function append (sourceBuffer, arrayBuffer) {
     sourceBuffer.appendBuffer(arrayBuffer)
   })
 }
-let alot = 10
-async function forever (task, wait) {
-  while (alot--) {
-    await task()
-    await new Promise((resolve) => {
-      setTimeout(resolve, wait)
-    })
-  }
+async function forever (task, wait) { // eslint-disable-line
+  await task()
+  await new Promise((resolve) => {
+    setTimeout(resolve, wait)
+  })
+}
+function wrapEvent (event) {
+  return new Event(event.type, { original: event })
 }
 
 // HLS manifest custom tag parser
 function btAttrListParser (line) {
-  const [tag, value] = line.split(':')
+  const value = line.split(':')[1]
   const entries = value.split(',')
   const attrs = {}
   entries.forEach((entry) => {
@@ -65,8 +66,9 @@ const SEGMENT_PARSER = {
   dataParser: btAttrListParser
 }
 
-export default class DemoPlayer {
+export default class DemoPlayer extends EventTarget {
   constructor (video, srcUrl) {
+    super()
     this._srcUrl = srcUrl
     this._video = video
     this._livetorrent = new LiveTorrent()
@@ -79,51 +81,64 @@ export default class DemoPlayer {
     this._setup()
   }
 
+  redispatch (event) {
+    return this.dispatchEvent(wrapEvent(event))
+  }
+
   async _setup () {
     const mediaSource = await attachMediaSource(this._video)
     const sourceBuffer = mediaSource.addSourceBuffer('video/mp4;codecs="mp4a.40.2,avc1.4d401e"')
 
-    forever(async () => {
-      const response = await fetch(this._srcUrl)
-      if (!response.ok) {
-        throw new Error(`Status ${response.statusCode} from "${this._srcUrl}": ${response.statusText}`)
+    mediaSource.addEventListener('sourceopen', this.redispatch.bind(this))
+    mediaSource.addEventListener('sourceended', this.redispatch.bind(this))
+    mediaSource.addEventListener('sourceclose', this.redispatch.bind(this))
+
+    // forever(async () => {
+    const response = await fetch(this._srcUrl)
+    if (!response.ok) {
+      throw new Error(`Status ${response.statusCode} from "${this._srcUrl}": ${response.statusText}`)
+    }
+    this._parser.push(await response.text())
+    this._parser.end()
+
+    // order is important so that the appends happen in the right
+    // sequence
+    const files = [{
+      hash: this._parser.manifest.custom.bt.hash,
+      length: this._parser.manifest.custom.bt.length,
+      uri: this._parser.manifest.segments[0].map.uri
+    }].concat(this._parser.manifest.segments.map((segment) => {
+      return {
+        hash: segment.custom.bt.hash,
+        length: segment.custom.bt.length,
+        uri: segment.uri
       }
-      this._parser.push(await response.text())
-      this._parser.end()
-
-      // order is important so that the appends happen in the right
-      // sequence
-      const files = [{
-        hash: this._parser.manifest.custom.bt.hash,
-        length: this._parser.manifest.custom.bt.length,
-        uri: this._parser.manifest.segments[0].map.uri
-      }].concat(this._parser.manifest.segments.map((segment) => {
-        return {
-          hash: segment.custom.bt.hash,
-          length: segment.custom.bt.length,
-          uri: segment.uri
+    }))
+    for (const file of files) {
+      if (this._segments.has(file.uri)) {
+        // we've already seen this file
+        continue
+      }
+      this._segments.add(file.uri)
+      this._fetches = this._fetches.then(async () => {
+        const response = await this._livetorrent.fetch(file.uri)
+        if (!response.ok) {
+          throw new Error(`Download failed for "${file.uri}"`)
         }
-      }))
-      for (const file of files) {
-        if (this._segments.has(file.uri)) {
-          // we've already seen this file
-          continue
-        }
-        this._segments.add(file.uri)
-        this._fetches = this._fetches.then(async () => {
-          const response = await this._livetorrent.fetch(file.uri)
-          if (!response.ok) {
-            throw new Error(`Download failed for "${file.uri}"`)
-          }
-          const data = await response.arrayBuffer()
+        const data = await response.arrayBuffer()
 
-          console.log(`appending ${file.uri}, ${data.byteLength} bytes`)
-          return append(sourceBuffer, data)
+        return append(sourceBuffer, data).then(() => {
+          this.dispatchEvent(new Event('progress'))
         })
-      }
+      })
+    }
 
-      const baseUrl = this._srcUrl.split('/').slice(0, -1).join('/')
-      this._livetorrent.update(baseUrl, files)
-    }, 500)
+    this._fetches = this._fetches.then(() => {
+      mediaSource.endOfStream()
+    })
+
+    const baseUrl = this._srcUrl.split('/').slice(0, -1).join('/')
+    this._livetorrent.update(baseUrl, files)
+    // }, 500)
   }
 }
