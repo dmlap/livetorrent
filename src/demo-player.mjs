e2d939d2a1123166f3c52df4bcb04ddd6b378b22
Dave LaPalomento <dlapalomento@gmail.com>
2021-01-20 11:53:04 -0500

    Web seed buffering with simulated live stream Load a simulated live stream through web seed peers successfully. Still some open questions about how to handle real peers and piece-indexing.

--- src/demo-player.mjs
+++ src/demo-player.mjs
@@ -3,6 +3,31 @@
 import '../../node_modules/m3u8-parser/dist/m3u8-parser.js'
 import LiveTorrent from './index.js'
 
+/**
+ * Utility method for debugging.
+ */
+function hexdump (arrayBuffer) {
+  const result = []
+  const stride = 16
+  for (let i = 0; i < arrayBuffer.byteLength; i += stride) {
+    const rowLength = Math.min(arrayBuffer.byteLength - i, stride)
+    const bytes = new Uint8Array(arrayBuffer, i, rowLength)
+
+    const row = [i.toString(16).padStart(8, '0') + ':']
+    for (let j = 0; j < rowLength - 2; j += 2) {
+      row.push(bytes[j].toString(16).padStart(2, '0') + bytes[j + 1].toString(16).padStart(2, '0'))
+    }
+    if (rowLength % 2 === 0) {
+      row.push(bytes[rowLength - 2].toString(16).padStart(2, '0') + bytes[rowLength - 1].toString(16).padStart(2, '0'))
+    } else {
+      row.push(bytes[rowLength - 1].toString(16).padStart(2, '0') + '  ')
+    }
+    result.push(row.join(' '))
+  }
+  return result.join('\n')
+}
+
+// --- Utiltity methods for working with Media Source Extensions ---
 function attachMediaSource (videoEl) {
   return new Promise((resolve, reject) => {
     const mediaSource = new MediaSource()
@@ -27,14 +52,13 @@ function append (sourceBuffer, arrayBuffer) {
     sourceBuffer.appendBuffer(arrayBuffer)
   })
 }
-async function forever (task, wait) { // eslint-disable-line
-  await task()
-  await new Promise((resolve) => {
-    setTimeout(resolve, wait)
-  })
-}
-function wrapEvent (event) {
-  return new Event(event.type, { original: event })
+async function doWhile (task, test, wait) {
+  do {
+    await task()
+    await new Promise((resolve) => {
+      setTimeout(resolve, wait)
+    })
+  } while (await test())
 }
 
 // HLS manifest custom tag parser
@@ -71,74 +95,78 @@ export default class DemoPlayer extends EventTarget {
     super()
     this._srcUrl = srcUrl
     this._video = video
-    this._livetorrent = new LiveTorrent()
+    this._livetorrent = new LiveTorrent(this._srcUrl.split('/').slice(0, -1).join('/'))
     this._fetches = Promise.resolve()
     this._segments = new Set()
-    this._parser = new m3u8Parser.Parser()
-    this._parser.addParser(MAP_PARSER)
-    this._parser.addParser(SEGMENT_PARSER)
+    this._targetDuration = 6 / 3
+
+    this._logicalTime = 0
 
     this._setup()
   }
 
-  redispatch (event) {
-    return this.dispatchEvent(wrapEvent(event))
+  _redispatch (event) {
+    return this.dispatchEvent(new Event(event.type, { original: event }))
   }
 
   async _setup () {
     const mediaSource = await attachMediaSource(this._video)
     const sourceBuffer = mediaSource.addSourceBuffer('video/mp4;codecs="mp4a.40.2,avc1.4d401e"')
 
-    mediaSource.addEventListener('sourceopen', this.redispatch.bind(this))
-    mediaSource.addEventListener('sourceended', this.redispatch.bind(this))
-    mediaSource.addEventListener('sourceclose', this.redispatch.bind(this))
+    mediaSource.addEventListener('sourceopen', this._redispatch.bind(this))
+    mediaSource.addEventListener('sourceended', this._redispatch.bind(this))
+    mediaSource.addEventListener('sourceclose', this._redispatch.bind(this))
 
-    // forever(async () => {
-    const response = await fetch(this._srcUrl)
-    if (!response.ok) {
-      throw new Error(`Status ${response.statusCode} from "${this._srcUrl}": ${response.statusText}`)
-    }
-    this._parser.push(await response.text())
-    this._parser.end()
+    doWhile(async () => {
+      const response = await fetch(`example/live${this._logicalTime}.m3u8`) // this._srcUrl)
+      this._logicalTime++
 
-    // order is important so that the appends happen in the right
-    // sequence
-    const files = [{
-      hash: this._parser.manifest.custom.bt.hash,
-      length: this._parser.manifest.custom.bt.length,
-      uri: this._parser.manifest.segments[0].map.uri
-    }].concat(this._parser.manifest.segments.map((segment) => {
-      return {
-        hash: segment.custom.bt.hash,
-        length: segment.custom.bt.length,
-        uri: segment.uri
+      if (!response.ok) {
+        throw new Error(`Status ${response.statusCode} from "${this._srcUrl}": ${response.statusText}`)
       }
-    }))
-    for (const file of files) {
-      if (this._segments.has(file.uri)) {
-        // we've already seen this file
-        continue
-      }
-      this._segments.add(file.uri)
-      this._fetches = this._fetches.then(async () => {
-        const response = await this._livetorrent.fetch(file.uri)
-        if (!response.ok) {
-          throw new Error(`Download failed for "${file.uri}"`)
+      const parser = new m3u8Parser.Parser()
+      parser.addParser(MAP_PARSER)
+      parser.addParser(SEGMENT_PARSER)
+      parser.push(await response.text())
+      parser.end()
+      this._targetDuration = (parser.manifest.targetDuration / 2)
+
+      // the segment fetches (and appends) must be correctly ordered
+      const files = [{
+        hash: parser.manifest.custom.bt.hash,
+        length: parser.manifest.custom.bt.length,
+        uri: parser.manifest.segments[0].map.uri
+      }].concat(parser.manifest.segments.map((segment) => {
+        return {
+          hash: segment.custom.bt.hash,
+          length: segment.custom.bt.length,
+          uri: segment.uri
         }
-        const data = await response.arrayBuffer()
+      }))
+      for (const file of files) {
+        if (this._segments.has(file.uri)) {
+          // we've already seen this file
+          continue
+        }
+        this._segments.add(file.uri)
+        this._fetches = this._fetches.then(async () => {
+          const response = await this._livetorrent.fetch(file.uri)
+          if (!response.ok) {
+            throw new Error(`Download failed for "${file.uri}"`)
+          }
+          const data = await response.arrayBuffer()
+          console.log(file.uri + '\n' + hexdump(data).slice(0, (12 + 16 + 1) * 5))
 
-        return append(sourceBuffer, data).then(() => {
-          this.dispatchEvent(new Event('progress'))
+          return append(sourceBuffer, data).then(() => {
+            console.log(`appended ${data.byteLength} bytes of ${file.uri}`)
+            this.dispatchEvent(new Event('progress'))
+          })
         })
-      })
-    }
+      }
 
-    this._fetches = this._fetches.then(() => {
-      mediaSource.endOfStream()
-    })
-
-    const baseUrl = this._srcUrl.split('/').slice(0, -1).join('/')
-    this._livetorrent.update(baseUrl, files)
-    // }, 500)
+      this._livetorrent.update(files)
+    }, () => {
+      return this._logicalTime < 5
+    }, this._targetDuration * 1000)
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,74 +2,190 @@
 
 const PIECE_LENGTH = Math.pow(2, 18)
 
+function buildParsedTorrent (baseUrl, files) {
+  const torrent = {
+    // empty file SHA-1 hash
+    infoHash: 'da39a3ee5e6b4b0d3255bfef95601890afd80709',
+    comment: `LiveTorrent from "${baseUrl}"`,
+    urlList: [baseUrl],
+    pieceLength: PIECE_LENGTH,
+    lastPieceLength: PIECE_LENGTH,
+    pieces: [],
+    files: [],
+    info: {
+      name: baseUrl.split('/').slice(-1)[0]
+    }
+  }
+  let offset = 0
+  for (const file of files) {
+    torrent.files.push({
+      length: file.length,
+      path: file.uri,
+      name: file.uri,
+      offset
+    })
+    offset += file.length
+    const padLength = PIECE_LENGTH - (file.length % PIECE_LENGTH)
+    if (padLength !== 0) {
+      torrent.files.push({
+        attr: 'p',
+        length: padLength,
+        path: '.pad/' + padLength,
+        name: '' + padLength,
+        offset
+      })
+      offset += padLength
+    }
+    torrent.pieces.push(file.hash)
+  }
+  torrent.info.files = torrent.files
+  torrent.info['piece length'] = torrent.pieceLength
+  torrent.info.pieces = new Uint8Array(torrent.pieces.join('').match(/[\da-f]{2}/gi).map((pair) => {
+    return parseInt(pair, 16)
+  }))
+  return torrent
+}
+
 export default class LiveTorrent {
-  constructor () {
+  constructor (baseUrl) {
+    this._baseUrl = baseUrl
     this._webtorrent = new WebTorrent()
+
+    this._knownHashes = new Set()
+    this._torrent = null
+    this._File = null
+    this._Piece = null
     this._ready = new Promise((resolve, reject) => {
-      this._webtorrent.on('ready', resolve)
+      this._webtorrent.on('torrent', (torrent) => {
+        if (this._torrent) return
+
+        this._torrent = torrent
+        this._File = Object.getPrototypeOf(torrent.files[0]).constructor
+        this._Piece = Object.getPrototypeOf(torrent.pieces[0]).constructor
+        resolve()
+      })
       this._webtorrent.on('error', reject)
       this._webtorrent.on('close', reject)
     })
-
-    // FIXME prevent multiple updates to the webtorrent client while
-    // figuring out how to bend it into shape
-    this._done = false
   }
 
-  update (baseUrl, files) {
-    if (this._done) {
-      return
-    }
-
-    const torrent = {
-      // empty file SHA-1 hash
-      infoHash: 'da39a3ee5e6b4b0d3255bfef95601890afd80709',
-      comment: 'Live Torrent',
-      urlList: [baseUrl],
-      pieceLength: PIECE_LENGTH,
-      lastPieceLength: PIECE_LENGTH,
-      pieces: [],
-      files: [],
-      info: {
-        name: baseUrl.split('/').slice(-1)[0]
-      }
-    }
-    let offset = 0
+  /**
+   * Setup a torrent that will be used for streaming.
+   */
+  _createTorrent (files) {
+    const torrent = buildParsedTorrent(this._baseUrl, files)
     for (const file of files) {
-      torrent.files.push({
-        length: file.length,
-        path: file.uri,
-        name: file.uri,
-        offset
-      })
-      offset += file.length
-      const padLength = PIECE_LENGTH - (file.length % PIECE_LENGTH)
-      if (padLength !== 0) {
-        torrent.files.push({
-          attr: 'p',
-          length: padLength,
-          path: '.pad/' + padLength,
-          name: '' + padLength,
-          offset
-        })
-        offset += padLength
-      }
-      torrent.pieces.push(file.hash)
+      this._knownHashes.add(file.hash)
     }
-    torrent.info.files = torrent.files
-    torrent.info['piece length'] = torrent.pieceLength
-    torrent.info.pieces = new Uint8Array(torrent.pieces.join('').match(/[\da-f]{2}/gi).map((pair) => {
-      return parseInt(pair, 16)
-    }))
 
     this._webtorrent.add(torrent)
-    this._done = true
+  }
+
+  /**
+   * Update torrent and web seeds with any files that aren't already
+   * tracked by the torrent.
+   */
+  _updateTorrent (files) {
+    // process & update metadata
+    // TODO skipping this because it mutates the torrent object. It
+    // would be good hygiene to update magnetURI and torrentFile
+    // this._processParsedTorrent(parsedTorrent)
+    // this.metadata = this.torrentFile
+
+    const torrent = this._torrent
+    const updateIx = files.findIndex((file) => {
+      return !this._knownHashes.has(file.hash)
+    })
+    const parsedTorrent = buildParsedTorrent(
+      this._baseUrl, files.slice(updateIx)
+    )
+    const additions = parsedTorrent.files
+    if (additions.length === 0) {
+      return
+    }
+    let offset = torrent.pieceLength * (torrent.pieces.length - 1) + torrent.lastPieceLength
+    for (const addition of additions) {
+      // update the file offsets to append the additions to the end of
+      // the torrent
+      addition.offset = offset
+      offset += addition.length
+      // append the content and padding files
+      torrent.files.push(new this._File(torrent, addition))
+    }
+    for (let i = updateIx; i < files.length; i++) {
+      // register the padded hash so it isn't added again
+      this._knownHashes.add(files[i].hash)
+      // add the new hashes to the torrent
+      torrent._hashes.push(files[i].hash)
+    }
+
+    // update rarity map
+    torrent._rarityMap._pieces.length += additions.length
+    torrent._rarityMap._numPieces = this._torrent._rarityMap._pieces.length
+    torrent._rarityMap.recalculate()
+
+    // update the chunk store
+    // this assumes the chunk store is `memory-chunk-store` (the
+    // default in a browser)
+    // at minimum, internal file information would have to be updated
+    // to use the default for node.js, FSChunkStore
+    const lastFile = additions.slice(-1)[0]
+    // `torrent._store.length` is read-only because `_store` is a
+    // Function. Even though it is treated in the constructor as a
+    // byte count, it appears safe to ignore updating it.
+    torrent._store.lastChunkLength = Math.min(lastFile.length, PIECE_LENGTH)
+    torrent._store.lastChunkIndex =
+      Math.ceil((lastFile.offset + lastFile.length) / PIECE_LENGTH) - 1
+
+    // add new pieces
+    for (let i = 0; i < parsedTorrent.pieces.length; i++) {
+      torrent.pieces.push(new this._Piece(PIECE_LENGTH))
+    }
+
+    // increase the range of the torrent bitfield
+    torrent.bitfield.grow = parsedTorrent.pieces.length
+
+    // update metadata on wires
+    torrent.wires.forEach((wire) => {
+      // If we didn't have the metadata at the time ut_metadata was initialized for this
+      // wire, we still want to make it available to the peer in case they request it.
+      if (wire.ut_metadata) wire.ut_metadata.setMetadata(torrent.metadata)
+
+      if (wire.type === 'webSeed') {
+        for (let i = 0; i <= torrent.pieces.length; i++) {
+          wire.peerPieces.set(i, true)
+        }
+      } else {
+        wire.bitfield(torrent.bitfield)
+      }
+    })
+
+    // now that the torrent state is updated, trigger interest in the
+    // new files
+    for (let i = torrent.files.length - additions.length; i < torrent.files.length; i++) {
+      // add new files to selection list
+      if (!torrent.so || torrent.so.includes(i)) {
+        torrent.files[i].select()
+      } else {
+        torrent.files[i].deselect()
+      }
+      // update reservations
+      torrent._reservations[i] = []
+    }
+  }
+
+  update (files) {
+    if (!this._torrent) {
+      return this._createTorrent(files)
+    } else {
+      return this._updateTorrent(files)
+    }
   }
 
   async fetch (url, init) {
     await this._ready
 
-    const file = this._webtorrent.torrents[0].files.find((file) => {
+    const file = this._torrent.files.find((file) => {
       return file.path === url
     })
 
